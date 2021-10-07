962266249c9766f8ccffd7c6880627a32d024e4e
jacques <jacques.perrault@branch.technology>
2021-10-07 17:03:54 -0400

    added zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
 // import {WebViewer} = Communicator.WebViewer;
 /// <reference path="./hoops_web_viewer.d.ts"/>
 
-function App() {
+import React, {useEffect, useLayoutEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
 
+import create, {SetState, GetState} from 'zustand'
+
+
+export type RootState = {
+    viewer?: Communicator.WebViewer;
+    setViewer: (id: string) => void
+}
+
+const createScene = async (containerId: string): Promise<Communicator.WebViewer> => {
+    const viewer = await new window.Communicator.WebViewer({
+        containerId: "canvas",
+        endpointUri: process.env.PUBLIC_URL + "input.scs",
+    });
+    viewer.start();
+    return viewer
+}
+
+const useStore = create<RootState>((set) => ({
+    viewer: undefined,
+    setViewer: async (id: string) => set({viewer: await createScene(id)}),
+}));
+
+type Props = {
+};
+
+export function Canvas(props: Props) {
+    const setViewer = useStore(state => state.setViewer)
     useEffect(() => {
-        // @ts-ignore
-        const _viewer = new window.Communicator.WebViewer({
-            containerId: "canvas",
-            endpointUri: process.env.PUBLIC_URL + "input.scs",
-        });
-        _viewer.start();
-        return () => {
-
-        };
+        console.log("setting up scene")
+        setViewer("canvas");
     }, []);
 
-  return (
-    <div id="canvas" className="App">
+    return (
+        <div id="canvas" className="App"/>
+    );
+};
 
-    </div>
-  );
+function App() {
+
+    return (
+        <>
+            <Canvas />
+        </>
+    );
 }
 
 export default App;
