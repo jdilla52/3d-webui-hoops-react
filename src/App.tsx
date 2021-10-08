6ae210971908caef4119dc74ae9bbdc9fcac1b5d
jacques <jacques.perrault@branch.technology>
2021-10-08 11:53:15 -0400

    used ref for container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 // import {WebViewer} = Communicator.WebViewer;
 /// <reference path="./hoops_web_viewer.d.ts"/>
 
-import React, {useEffect, useLayoutEffect} from 'react';
-import logo from './logo.svg';
+import React, {PropsWithChildren, useEffect} from 'react';
 import './App.css';
 
 import create, {SetState, GetState} from 'zustand'
@@ -10,12 +9,12 @@ import create, {SetState, GetState} from 'zustand'
 
 export type RootState = {
     viewer?: Communicator.WebViewer;
-    setViewer: (id: string) => void
+    setViewer: (id: HTMLElement) => void
 }
 
-const createScene = async (containerId: string): Promise<Communicator.WebViewer> => {
+const createScene = async (container:HTMLElement): Promise<Communicator.WebViewer> => {
     const viewer = await new window.Communicator.WebViewer({
-        containerId: "canvas",
+        container,
         endpointUri: process.env.PUBLIC_URL + "input.scs",
     });
     viewer.start();
@@ -24,29 +23,43 @@ const createScene = async (containerId: string): Promise<Communicator.WebViewer>
 
 const useStore = create<RootState>((set) => ({
     viewer: undefined,
-    setViewer: async (id: string) => set({viewer: await createScene(id)}),
+    setViewer: async (id: HTMLElement) => set({viewer: await createScene(id)}),
 }));
 
 type Props = {
+
 };
 
-export function Canvas(props: Props) {
+export const Canvas: React.FC<PropsWithChildren<Props>> = ({children})=> {
     const setViewer = useStore(state => state.setViewer)
+    const element = React.useRef<HTMLElement | null>(null)
+    const setRef = (node: HTMLElement | null) => {
+        if (!node || element.current) {
+            return
+        }
+        element.current = node
+    }
+
     useEffect(() => {
         console.log("setting up scene")
-        setViewer("canvas");
-    }, []);
+        if(element.current){
+            setViewer(element.current);
+        }
+    }, [element]);
 
     return (
-        <div id="canvas" className="App"/>
+        <div id="canvas" ref={setRef} className="App"/>
     );
 };
+
 
 function App() {
 
     return (
         <>
-            <Canvas />
+            <Canvas>
+                <div>sdf</div>
+            </Canvas>
         </>
     );
 }
