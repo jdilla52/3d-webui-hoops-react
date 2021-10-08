a21e4c792b6a94eb2399eda2cff22a2e3a3c375c
jacques <jacques.perrault@branch.technology>
2021-10-08 13:53:12 -0400

    example scs object

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import {WebViewer} = Communicator.WebViewer;
 /// <reference path="./hoops_web_viewer.d.ts"/>
 
-import React, {PropsWithChildren, useEffect} from 'react';
+import React, {PropsWithChildren, useEffect, useState} from 'react';
 import './App.css';
 
 import create, {SetState, GetState} from 'zustand'
@@ -12,25 +12,37 @@ export type RootState = {
     setViewer: (id: HTMLElement) => void
 }
 
-const createScene = async (container:HTMLElement): Promise<Communicator.WebViewer> => {
+const createScene = async (container: HTMLElement): Promise<Communicator.WebViewer> => {
     const viewer = await new window.Communicator.WebViewer({
         container,
-        endpointUri: process.env.PUBLIC_URL + "input.scs",
     });
     viewer.start();
+    await new Promise<void>((resolve) => {
+        viewer.setCallbacks({
+            sceneReady: () => {
+                viewer.view.setBackgroundColor(Communicator.Color.createFromFloatArray([0.121, 0.129, 0.149]), Communicator.Color.createFromFloatArray([0.929, 0.933, 0.952]));
+
+                },
+            modelStructureReady: () => {
+                resolve();
+            },
+
+            selectionArray: (v, a) => {
+            },
+        });
+        return viewer
+    });
     return viewer
 }
 
-const useStore = create<RootState>((set) => ({
+const useStore = create<RootState>((set, get) => ({
     viewer: undefined,
     setViewer: async (id: HTMLElement) => set({viewer: await createScene(id)}),
 }));
 
-type Props = {
+type Props = {};
 
-};
-
-export const Canvas: React.FC<PropsWithChildren<Props>> = ({children})=> {
+export const Canvas: React.FC<PropsWithChildren<Props>> = ({children}) => {
     const setViewer = useStore(state => state.setViewer)
     const element = React.useRef<HTMLElement | null>(null)
     const setRef = (node: HTMLElement | null) => {
@@ -42,23 +54,46 @@ export const Canvas: React.FC<PropsWithChildren<Props>> = ({children})=> {
 
     useEffect(() => {
         console.log("setting up scene")
-        if(element.current){
+        if (element.current) {
             setViewer(element.current);
         }
     }, [element]);
 
     return (
-        <div id="canvas" ref={setRef} className="App"/>
+        <div id="canvas" ref={setRef} className="App">
+            {children}
+        </div>
     );
 };
 
+type ScsUrlObjectProps = {
+    url: string;
+}
+
+export const ScsUrlObject: React.FC<PropsWithChildren<ScsUrlObjectProps> > = ({url}) =>{
+    const viewer = useStore(state => state.viewer);
+    useEffect(() => {
+        if (viewer) {
+            const n = viewer.model.createNode(-2, "");
+            const _ = viewer.model.loadSubtreeFromScsFile(n, url);
+            return () => {
+                viewer?.model.deleteNode(n);
+            };
+        }
+    }, [viewer, url]);
+
+    return (
+       <></>
+    );
+};
 
 function App() {
-
+    const [show, setShow] = useState(false);
     return (
         <>
             <Canvas>
-                <div>sdf</div>
+                {show ? <ScsUrlObject url={process.env.PUBLIC_URL + "input.scs"}/> : null}
+                <button onClick={()=> setShow(!show)}>show</button>
             </Canvas>
         </>
     );
